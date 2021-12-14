b6c6c234d2baf549ea1a2531071b1ae7a3c69856
hera-zeus <53087634+hera-zeus@users.noreply.github.com>
2021-12-14 09:32:15 +0100

    Update __manifest__.py

--- turnover/__manifest__.py
+++ turnover/__manifest__.py
@@ -2,18 +2,9 @@
 ##############################################################################
 #
 #    OpenERP, Open Source Management Solution
-#    Copyright (C) 2021 Normalis Consulting (Merveille ZEBAZE) 
-#    (<http://normalis.consulting>).
+#    Copyright (C) 2021 Merveille ZEBAZE
+#    (<http://merveillezebaze.pythonanywhere.com>).
 #
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
 #
 ##############################################################################
 {
@@ -25,10 +16,9 @@
     'description': """
     Back Dated Stock Take Report allows to print stock position corresponding to a specific date in the past.
 """,
-    'author' : 'Normalis Consulting (Merveille ZEBAZE) ',
-    'website' : 'http://normalis.consulting',
+    'author' : 'Merveille ZEBAZE',
+    'website' : 'http://merveillezebaze.pythonanywhere.com/',
     'depends': ['stock'],
-    #'images': ['static/description/banner.jpg'],
     'data': [
         'wizard/print_turnover_report_view.xml',
         'wizard/print_turnover_pdf.xml',
